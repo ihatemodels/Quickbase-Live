ab6b1a70e891ea9b8eafb3a4a8c5d79dd427b6e2
ihatemodels <ihatemodels1@proton.me>
2023-09-30 01:04:26 +0300

    - add stuff

--- iac/main.tf
+++ iac/main.tf
@@ -7,45 +7,48 @@ resource "google_project_service" "domains" {
 }
 
 # Deploy the initial revision of the service
-resource "google_cloud_run_service" "qb-demo" {
+resource "google_cloud_run_v2_service" "qb-demo" {
   name     = "quickbase"
   location = "europe-north1"
 
   template {
-    spec {
-      containers {
-        image = "st3ga/quickbase-demo:1.1.0"
-        startup_probe {
-          initial_delay_seconds = 0
-          timeout_seconds       = 1
-          period_seconds        = 3
-          failure_threshold     = 1
-          tcp_socket {
-            port = 80
-          }
+    scaling {
+      max_instance_count = 2
+    }
+    containers {
+      image = "st3ga/quickbase-demo:v1.3.0"
+      startup_probe {
+        initial_delay_seconds = 3
+        timeout_seconds       = 1
+        period_seconds        = 3
+        failure_threshold     = 2
+        tcp_socket {
+          port = 8080
         }
-        liveness_probe {
-          http_get {
-            path = "/healthz"
-          }
+      }
+      liveness_probe {
+        http_get {
+          path = "/healthz"
         }
-        ports {
-          container_port = 80
-        }
+      }
+      ports {
+        container_port = 8080
       }
     }
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+  depends_on = [
+    google_project_service.cloudrun,
+    google_project_service.domains
+  ]
 }
 
+
 # Make the service public
-resource "google_cloud_run_service_iam_member" "public" {
-  service  = google_cloud_run_service.qb-demo.name
-  location = "europe-north1"
+resource "google_cloud_run_service_iam_binding" "public" {
+  location = google_cloud_run_v2_service.qb-demo.location
+  service  = google_cloud_run_v2_service.qb-demo.name
   role     = "roles/run.invoker"
-  member   = "allUsers"
+  members = [
+    "allUsers"
+  ]
 }
